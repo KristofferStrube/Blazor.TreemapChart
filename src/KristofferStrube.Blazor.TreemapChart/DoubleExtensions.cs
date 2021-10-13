44db0ad76047c50715bc1c1ac094cf5c2e0c8a5a
Kristoffer Strube <kristoffer.strube@gmail.com>
2021-10-13 03:10:38 +0200

    added projects

---
+++ src/KristofferStrube.Blazor.TreemapChart/DoubleExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KristofferStrube.Blazor.TreemapChart
+{
+    public static class DoubleExtensions
+    {
+        public static string AsString(this double d) => d.ToString(CultureInfo.InvariantCulture);
+    }
+}
